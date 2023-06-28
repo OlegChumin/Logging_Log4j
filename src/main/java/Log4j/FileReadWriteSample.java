7b125f85da32459f6786dc562174b47f64f915dc
OlegChumin <tschumin.oleg@gmail.com>
2023-06-28 08:14:22 +0300

    Add new files

--- src/main/java/Log4j/FileReadWriteSample.java
+++ src/main/java/Log4j/FileReadWriteSample.java
@@ -3,16 +3,13 @@ package Log4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class FileReadWriteSample {
     private final static Logger logger = LogManager.getLogger(FileReadWriteSample.class);
 
     public static void main(String[] args) {
-        String filePath = "file.txt";
+        String filePath = "src/main/resources/file.txt";
         String newText = "This is new text to be written to the file.";
 
         // reading from file.txt
@@ -26,6 +23,13 @@ public class FileReadWriteSample {
             throw new RuntimeException(e);
         } catch (IOException e) {
             logger.error("Error in reading file: " + e.getMessage());
+        }
+
+        //writing new data
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath, true));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
